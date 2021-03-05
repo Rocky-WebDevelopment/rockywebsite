61192b9bb5a48cb2da60941c6cfe9006fc7acdb4
nicmolina <nicolasmarcondes99@gmail.com>
2021-03-05 11:59:00 -0300

    change on git ignore

--- src/components/Forms/index.js
+++ src/components/Forms/index.js
@@ -6,15 +6,17 @@ function Forms(){
 
     return(
         <div className="forms-container">
-            <label >Nome</label>
-            <input placeholder="Guilherme Quintal"></input>
-            <label>E-mail</label>
-            <input placeholder="guilherme@gmail.com"></input>
-            <label>Investimento</label>
-            <input placeholder="R$"></input>
-            <label>Mensagem</label>
-            <input id="message" placeholder="Bom dia"></input>
-            <button>ENVIAR MENSAGEM</button>
+            <div className="forms-background">
+                <label >Nome</label>
+                <input placeholder="Guilherme Quintal"></input>
+                <label>E-mail</label>
+                <input placeholder="guilherme@gmail.com"></input>
+                <label>Investimento</label>
+                <input placeholder="R$"></input>
+                <label>Mensagem</label>
+                <input id="message" placeholder="Bom dia"></input>
+                <button>ENVIAR MENSAGEM</button>
+            </div>
         </div>
     );
 }
