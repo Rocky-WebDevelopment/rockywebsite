2a43f28b1bc1587214438e27d8459e14a6b6a3a3
nicmolina <nicolasmarcondes99@gmail.com>
2021-03-05 16:23:30 -0300

    adding new content

--- src/pages/CRO/PageCro.js
+++ src/pages/CRO/PageCro.js
@@ -8,23 +8,48 @@ function PageCro() {
                 img="ImgInbound"
             />
             <div className="container">
-                <h1>Inbound - CRO</h1>
-                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, harum </h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quibusdam suscipit a voluptatum id eius vel explicabo officia fugiat, ipsa, unde similique quia repudiandae. Iste unde repudiandae illo modi in?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid modi nisi nihil perferendis vero voluptas illo, possimus magni quaerat eaque omnis dolores tenetur iusto quod vel perspiciatis doloremque in et.</p>
+                <h1>CRO - Conversion Rate Optimization</h1>
+                <p>Já pensou por que o número de conversões do seu site está baixo mesmo tendo um bom tráfego? Com a implementação de uma estratégia de CRO é possível melhorar o desempenho da sua página e levar o usuário até uma ação positiva para seus resultados.</p>
+                <p>
+                    Nosso time de CRO é responsável por fazer uma análise técnica e comportamental do seu site, que através de um conjunto de práticas e otimizações, visa aumentar suas conversões. 
+                </p>
+                <p>
+                    Com uma visão heurística, ferramentas analíticas, testes comportamentais e análise de dados, geramos insights estratégicos para melhorar seus resultados e a experiência de usuário do seu cliente.
+                </p>
+                <h3>Metodologia</h3>
+                <p>
+                    Nosso time de especialistas em CRO trabalham com uma metodologia teórica e prática, em que analisam o site da marca e suas páginas principais para formular hipóteses iniciais sobre pontos de atenção em sua performance.
+                </p>
+                <p>
+                A partir dessas hipóteses, são desenvolvidos testes experimentais que serão executados na plataforma, para provar ou refutar essas teorias. Com essas implementações, há um acompanhamento preciso dos resultados em tempo real e assim, otimizações e melhorias são apresentadas.
+                </p>
             
-                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, harum </h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quibusdam suscipit a voluptatum id eius vel explicabo officia fugiat, ipsa, unde similique quia repudiandae. Iste unde repudiandae illo modi in?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid modi nisi nihil perferendis vero voluptas illo, possimus magni quaerat eaque omnis dolores tenetur iusto quod vel perspiciatis doloremque in et.</p>
+                <h3>
+                    Uso de dados quantitativos e qualitativos
+                </h3>
+                <p>
+                    Em CRO, dados quantitativos e dados qualitativos têm a mesma importância e são igualmente relevantes para a performance do site e o número de conversões.
+                </p>
+                <p>
+                    Utilizando ferramentas referência no mercado, trabalhamos com uma análise combinada dessas informações para fazer um levantamento de hipóteses inicial. 
+                </p>
+
+                <p>
+                    Estudamos sobre a experiência do usuário no site, o caminho de conversão do cliente e realizamos um estudo de persona para entender melhor seu comportamento.
+                </p>
             
-                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, harum </h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quibusdam suscipit a voluptatum id eius vel explicabo officia fugiat, ipsa, unde similique quia repudiandae. Iste unde repudiandae illo modi in?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid modi nisi nihil perferendis vero voluptas illo, possimus magni quaerat eaque omnis dolores tenetur iusto quod vel perspiciatis doloremque in et.</p>
-            
-                <h3>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto, harum </h3>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quibusdam suscipit a voluptatum id eius vel explicabo officia fugiat, ipsa, unde similique quia repudiandae. Iste unde repudiandae illo modi in?</p>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid modi nisi nihil perferendis vero voluptas illo, possimus magni quaerat eaque omnis dolores tenetur iusto quod vel perspiciatis doloremque in et.</p>
-                
+                <p>
+                    Além disso, configuramos relatórios personalizados de performance, mapas de calor no site e fazemos uma priorização de testes a serem realizados de acordo com a necessidade identificada durante a análise.
+                </p>
+                <h3>
+                    Resultados
+                </h3>
+                <p>
+                    Com a finalização de todos os relatórios, estudos e testes, os resultados são analisados e documentados para concluir o estudo. Nosso time de CRO traz as melhores soluções para as páginas e apresenta as otimizações necessárias para uma melhor performance do site analisado.
+                </p>
+                <p>
+                    A implementação dessas melhorias também pode ser feita pelo nosso time.
+                </p>
             </div>
         </div>
     )
